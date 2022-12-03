ed6cf170cc1daaaa23a0e27f4baee8587bf1c712
jahedripon9 <jahedahmed3170@gmail.com>
2022-12-03 11:03:17 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import TheNav from "./components/Header/TheNav";
+import Sections from "./components/Sections/Sections";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./components/store/CartProvider";
+import TheFooter from "./components/Footer/TheFooter";
+import Swal from "sweetalert2";
 
-function App() {
+const App = () => {
+  //Using useState hook
+  const [cartIsShown, setCartIsShown] = useState(false);
+  //END
+
+  //Managing the state of the cart component using useState
+  const onShowCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const onCloseCartHandler = () => {
+    setCartIsShown(false);
+    
+  };
+  //END
+
+  //EVent to occur on Order
+  const onOrderHandler = () => {
+    setCartIsShown(false);
+
+    Swal.fire({
+      title: "Successful!",
+      text: "Your order is on the way",
+      icon: "success",
+    });
+  };
+
+  //END
+
+  //Rendering the cart and all the sections
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CartProvider>
+      {cartIsShown && (
+        <Cart onCloseCart={onCloseCartHandler} onOrder={onOrderHandler} />
+      )}
+      <TheNav onShowCart={onShowCartHandler} />
+      <Sections />
+      <TheFooter />
+    </CartProvider>
   );
-}
+  //END
+};
 
 export default App;
